4799152a91b56f6d75e0ed94e7c9478fa994752d
haraduka <kkinorinnn444@gmail.com>
2022-05-11 16:37:34 +0900

    fix lin vel reward to only x axis

--- flightlib/src/envs/vision_env/vision_env.cpp
+++ flightlib/src/envs/vision_env/vision_env.cpp
@@ -361,7 +361,7 @@ bool VisionEnv::computeReward(Ref<Vector<>> reward) {
 
   // - tracking a constant linear velocity
   Scalar lin_vel_reward =
-    vel_coeff_ * quad_state_.v.norm();
+    vel_coeff_ * quad_state_.v(QS::POSX);
 
   // - angular velocity penalty, to avoid oscillations
   const Scalar ang_vel_penalty = angular_vel_coeff_ * quad_state_.w.norm();
