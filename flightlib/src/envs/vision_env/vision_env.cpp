d10dc24a6e710522d7c32bd3593049e2e2a2cca7
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2023-03-06 15:30:53 +0900

    [REFACTORING] delete non-used variable

--- flightlib/src/envs/vision_env/vision_env.cpp
+++ flightlib/src/envs/vision_env/vision_env.cpp
@@ -421,7 +421,7 @@ Vector<visionenv::Theta_Cuts> VisionEnv::getsphericalboxel(
   const std::vector<Vector<3>, Eigen::aligned_allocator<Vector<3>>> &pos_b_list,
   const std::vector<Scalar> &obs_radius_list, const Vector<3> &poll_v,
   const Matrix<3, 3> &R_T) {
-  Vector<3> vel_2d = {quad_state_.v[0], quad_state_.v[1], 0};
+  // Vector<3> vel_2d = {quad_state_.v[0], quad_state_.v[1], 0};
   // Vector<3> body_vel = R_T * vel_2d;
   // Scalar vel_theta = std::atan2(body_vel[1], body_vel[0]);
   // Scalar vel_phi = std::atan(body_vel[2] / std::sqrt(std::pow(body_vel[0], 2)
