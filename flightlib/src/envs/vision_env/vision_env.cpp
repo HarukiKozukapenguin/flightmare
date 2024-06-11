08af65c699a4d5d88e296a23ad9fca0430638c06
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-06-11 19:02:27 +0900

    [flightlib] activate dynamic obstacle in vision_env.cpp

--- flightlib/src/envs/vision_env/vision_env.cpp
+++ flightlib/src/envs/vision_env/vision_env.cpp
@@ -312,34 +312,34 @@ bool VisionEnv::getObstacleState(
 
   // compute relative distance to dynamic obstacles
   std::vector<Vector<3>, Eigen::aligned_allocator<Vector<3>>> relative_pos;
-  // for (int i = 0; i < (int)dynamic_objects_.size(); i++) {
-  //   // compute relative position vector
-  //   Vector<3> delta_pos = dynamic_objects_[i]->getPos() - quad_state_.p;
-  //   relative_pos.push_back(delta_pos);
+  for (int i = 0; i < (int)dynamic_objects_.size(); i++) {
+    // compute relative position vector
+    Vector<3> delta_pos = dynamic_objects_[i]->getPos() - quad_state_.p;
+    relative_pos.push_back(delta_pos);
 
-  //   // compute relative distance
-  //   Scalar obstacle_dist = delta_pos.norm();
-  //   Scalar obstacle_2d_dist =
-  //     std::sqrt(std::pow(delta_pos[0], 2) + std::pow(delta_pos[1], 2));
-  //   // limit observation range
-  //   if (obstacle_dist > max_detection_range_) {
-  //     obstacle_dist = max_detection_range_;
-  //   }
-  //   relative_pos_norm_.push_back(obstacle_dist);
-  //   relative_2d_pos_norm_.push_back(obstacle_2d_dist);
+    // compute relative distance
+    Scalar obstacle_dist = delta_pos.norm();
+    Scalar obstacle_2d_dist =
+      std::sqrt(std::pow(delta_pos[0], 2) + std::pow(delta_pos[1], 2));
+    // limit observation range
+    if (obstacle_dist > max_detection_range_) {
+      obstacle_dist = max_detection_range_;
+    }
+    relative_pos_norm_.push_back(obstacle_dist);
+    relative_2d_pos_norm_.push_back(obstacle_2d_dist);
 
-  //   // store the obstacle radius
-  //   Scalar obs_radius = dynamic_objects_[i]->getScale()[0] / 2;
-  //   // due to think quadsize, change obs_radius to more smaller to
-  //   // move forword
-  //   obs_radius = obs_radius / 2;
-  //   obstacle_radius_.push_back(obs_radius);
+    // store the obstacle radius
+    Scalar obs_radius = dynamic_objects_[i]->getScale()[0];
+    // due to think quadsize, change obs_radius to more smaller to
+    // move forword
+    obs_radius = obs_radius / 4;
+    obstacle_radius_.push_back(obs_radius);
 
-  //   //
-  //   if (obstacle_2d_dist < obs_radius + quad_size_) {
-  //     is_collision_ = true;
-  //   }
-  // }
+    //
+    if (obstacle_2d_dist < obs_radius + quad_size_) {
+      is_collision_ = true;
+    }
+  }
 
   // std::cout << "get dynamic_objects_" << std::endl;
 
@@ -733,7 +733,7 @@ bool VisionEnv::step(Ref<Vector<>> act, Ref<Vector<>> obs,
   // std::cout << "z is " << quad_state_.p(quad_state_.IDX::POSZ) << std::endl;
 
   // simulate dynamic obstacles
-  // simDynamicObstacles(sim_dt_);
+  simDynamicObstacles(sim_dt_);
 
   // update observations
   getObs(obs);
@@ -1234,14 +1234,14 @@ bool VisionEnv::changeLevel() {
   // cfg_["environment"]["env_folder"] = (env_id_ + 1) % 500;
 
   // add dynamic objects
-  // std::string dynamic_object_yaml =
-  //   obstacle_cfg_path_ + std::string("/dynamic_obstacles.yaml");
-  // if (!configDynamicObjects(dynamic_object_yaml)) {
-  //   // std::cout << dynamic_object_yaml << std::endl;
-  //   logger_.error(
-  //     "Cannot config Dynamic Object Yaml. Something wrong with the config "
-  //     "file");
-  // }
+  std::string dynamic_object_yaml =
+    obstacle_cfg_path_ + std::string("/dynamic_obstacles.yaml");
+  if (!configDynamicObjects(dynamic_object_yaml)) {
+    // std::cout << dynamic_object_yaml << std::endl;
+    logger_.error(
+      "Cannot config Dynamic Object Yaml. Something wrong with the config "
+      "file");
+  }
 
   // add static objects
   static_object_csv_ =
@@ -1423,9 +1423,9 @@ bool VisionEnv::addQuadrotorToUnity(const std::shared_ptr<UnityBridge> bridge) {
   if (!quad_ptr_) return false;
   bridge->addQuadrotor(quad_ptr_);
 
-  // for (int i = 0; i < (int)dynamic_objects_.size(); i++) {
-  //   bridge->addDynamicObject(dynamic_objects_[i]);
-  // }
+  for (int i = 0; i < (int)dynamic_objects_.size(); i++) {
+    bridge->addDynamicObject(dynamic_objects_[i]);
+  }
 
   //
   bridge->setRenderOffset(unity_render_offset_);
