ba9a692a5c77e7f131628997fc736fbe6ff91b5e
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-08-05 22:08:06 +0900

    [debug][dynamics] insert variable to kdacc_ vector

--- flightlib/src/dynamics/quadrotor_dynamics.cpp
+++ flightlib/src/dynamics/quadrotor_dynamics.cpp
@@ -225,7 +225,9 @@ Scalar QuadrotorDynamics::generateRandomValue(){
   }
 
 bool QuadrotorDynamics::randomizeKdacc() {
-  kdacc_ = range_vel_gain_[0] + generateRandomValue() * (range_vel_gain_[1] - range_vel_gain_[0]);
+  Scalar xy_kdacc = range_vel_gain_[0] + generateRandomValue() * (range_vel_gain_[1] - range_vel_gain_[0]);
+  kdacc_[0] = xy_kdacc;
+  kdacc_[1] = xy_kdacc;
   return true;
 }
 
